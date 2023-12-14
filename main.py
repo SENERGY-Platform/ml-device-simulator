686b525ddeb9b1820b3543f3969f51144ab828bb
Christoph Schrade <schrade.christoph@gmail.com>
2023-12-14 16:35:51 +0100

    add data for frequency test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import json
 import pandas as pd 
 import os 
 
-file_path = "./Test-Data/Kühlschrank_Outlier_Test.pickle"
+file_path = "./Test-Data/Missing_Data_Test.pickle"
 
 def create_sample(power, time):
     return f"""
